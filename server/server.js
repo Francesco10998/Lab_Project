a040946f88860dee8a088b6156f3fe7fb32368a0
Francesco10998 <fe.1814878@studenti.uniroma1.it>
2024-01-07 18:54:59 +0100

    servaraggiornato

--- server/server.js
+++ server/server.js
@@ -34,25 +34,25 @@ app.get("/login", (req, res) => {
   utils.connection.query(QueryUsername, function (error, results, fields) {
     if (error) throw error;
     else{
-      if(results != null){
-        console.log('usernames exists: ', results);
+      if(results.lenght != 0){
+        console.log('usernames exists');
         //control the password
         QueryPassword = 'SELECT * FROM users WHERE username="'+username+'" AND '+'pass="'+password+'"';
         utils.connection.query(QueryPassword, function (error, results, fields) {
           if (error) throw error;
           else{
-            if(results != null){
+            if(results.lenght != 0){
               console.log('user authenticated');
               res.json({ "results": ["authenticated"] });
-              //close database connection
-              utils.connection.end(function(err) {
-                // The connection is terminated now
-                console.log('connection with database terminated')
-              });
             }else{
               console.log('password inserted is wrong');
               res.json({ "results": ["wrong password"] });
             }
+            //close database connection
+            utils.connection.end(function(err) {
+            // The connection is terminated now
+            console.log('connection with database terminated')
+            });
           }
         });
       }else{
@@ -66,7 +66,66 @@ app.get("/login", (req, res) => {
 
 //------------------ register page ------------------------
 app.get("/register", (req, res) => {
-  res.json({ "users": ["userOne","userTwo", "userThree"] });
+  //take the username ,email and password passate da react
+  var data = req.query;
+  //console.log(data)
+
+  username = data.username;
+  email = data.email;
+  password = data.password;
+  address = data.address
+  cell = data.cell
+  paymentMethod = data.paymentMethod
+
+  encoded_password = utils.sha256(password)
+
+  //control if the username is already used
+  QueryUsername = 'SELECT * FROM users WHERE username="'+username+'"';
+
+  //execute query
+  utils.connection.query(QueryUsername, function (error, results, fields) {
+    if (error) throw error;
+    else{
+      if(results.length != 0){//username already exist part
+        console.log('usernames already exists: ');
+        res.json({ "results": ["username_already_exists"] });
+      }
+      else{//ok username , check the email
+        QueryEmail = 'SELECT * FROM users WHERE email="'+email+'"';
+        utils.connection.query(QueryEmail, function (error, results, fields) {
+          if (error) throw error;
+          else{
+            if(results.length != 0){//email already exist part
+              console.log('email already exists: ');
+              res.json({ "results": ["email_already_exists"] });
+            }
+            else{//ok email, create the account in the database
+              QueryNewUser = 'INSERT INTO users (username, email, pass, address, phoneNumber, paymentMethod) VALUES ("'+username+'", "'+email+'", "'+encoded_password+'", "'+address+'","'+cell+'","'+paymentMethod+'")';
+              utils.connection.query(QueryNewUser, function (error, results, fields) {
+                if (error) throw error;
+                else{
+                  if(results.length != 0){//user inserted
+                    console.log('username inserted: ');
+                    res.json({ "results": ["user_inserted"] });
+                  }else{//user not inserted
+                    console.log('username not inserted, expected some problem: ');
+                    res.json({ "results": ["user_not_inserted"] });
+                  }
+                  //close database connection
+                  utils.connection.end(function(err) {
+                  // The connection is terminated now
+                  console.log('connection with database terminated')
+              });
+                }
+              });
+            }
+          }
+        });
+      }
+    }
+  });
+        
+       
 });
 
 //----------------- myoffers page -------------------------
