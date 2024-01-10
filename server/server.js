eba884d3e40c69af7eb348c8cd9dc584574bcf6d
Francesco10998 <fe.1814878@studenti.uniroma1.it>
2024-01-10 17:29:50 +0100

    newserver

--- server/server.js
+++ server/server.js
@@ -32,10 +32,7 @@ app.get("/home", (req, res) => {
 //------------------login page --------------------------
 app.post("/login", (req, res) => {
 
-  console.log('ciao');
   //take the username and password passate da react
-  var data = req.query;
-
   const { username, password } = req.body;
   console.log(username,password);
 
@@ -85,16 +82,10 @@ app.post("/login", (req, res) => {
 
 //------------------ register page ------------------------
 app.post("/register", (req, res) => {
-  //take the username ,email and password passate da react
-  var data = req.query;
-  //console.log(data)
 
-  username = data.username;
-  email = data.email;
-  password = data.password;
-  address = data.address
-  cell = data.cell
-  paymentMethod = data.paymentMethod
+  //take the username ,email and password passate da react
+  const { username, email, password } = req.body;
+  //console.log(data)
 
   encoded_password = utils.sha256(password)
 
@@ -119,7 +110,8 @@ app.post("/register", (req, res) => {
               res.json({ "results": ["email_already_exists"] });
             }
             else{//ok email, create the account in the database
-              QueryNewUser = 'INSERT INTO users (username, email, pass, address, phoneNumber, paymentMethod) VALUES ("'+username+'", "'+email+'", "'+encoded_password+'", "'+address+'","'+cell+'","'+paymentMethod+'")';
+              //QueryNewUser = 'INSERT INTO users (username, email, pass, address, phoneNumber, paymentMethod) VALUES ("'+username+'", "'+email+'", "'+encoded_password+'", "'+address+'","'+cell+'","'+paymentMethod+'")';
+              QueryNewUser = 'INSERT INTO users (username, email, pass) VALUES ("'+username+'", "'+email+'", "'+encoded_password+'")';
               utils.pool.query(QueryNewUser, function (error, results, fields) {
                 if (error) throw error;
                 else{
@@ -131,10 +123,10 @@ app.post("/register", (req, res) => {
                     res.json({ "results": ["user_not_inserted"] });
                   }
                   //close database connection
-                  utils.pool.end(function(err) {
+                  /*utils.pool.end(function(err) {
                   // The connection is terminated now
                   console.log('connection with database terminated')
-              });
+                  });*/
                 }
               });
             }
@@ -146,6 +138,148 @@ app.post("/register", (req, res) => {
         
        
 });
+//----------------- enter settings page ------------------------
+app.post("/settings", (req, res) => {
+
+  const { mode,username } = req.body;
+  //------when open setting return all the data react will put on the placeholder--------//
+  if(mode=="enter"){
+    //username = "feffo98";
+    //take all data of the user
+    QueryData = 'SELECT * FROM users WHERE username="'+username+'"';
+
+    //execute query
+    utils.pool.query(QueryData, function (error, results, fields) {
+      if (error){
+        res.json({ "data_user": ["user not found"] });
+        throw error;
+      }
+      else{
+        if(results.length!=0){
+          //debugging
+          console.log(results)
+          //send user data to client
+          res.json({ "data_user": [results] });
+        }
+      }
+    });
+
+  }else if(mode=="modify"){/////-------------modify user parameters-------------------
+    const{ modifyusername,modifypassword,modifyemail,modifyaddress,modifyphone,modifypayment,newUsername,newEmail,newPassword,newAddress,newPhone,newPayment}= req.body;
+
+    if(modifyusername==true){
+      QueryModifyUsername = "UPDATE users SET username = "+newUsername+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyUsername, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //notify the client
+            res.json({ "data_user": ["username modified"] });
+          }
+        }
+      });
+    }
+    if(modifypassword==true){
+      QueryModifyPassword = "UPDATE users SET pass = "+newPassword+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyPassword, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //send user data to client
+            res.json({ "data_user": ["password modified"] });
+          }
+        }
+      });
+    }
+    if(modifyemail==true){
+      QueryModifyEmail = "UPDATE users SET email = "+newEmail+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyEmail, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //send user data to client
+            res.json({ "data_user": ["email modified"] });
+          }
+        }
+      });
+    }
+    if(modifyaddress==true){
+      QueryModifyAddress = "UPDATE users SET address = "+newAddress+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyAddress, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //send user data to client
+            res.json({ "data_user": [results] });
+          }
+        }
+      });
+    }
+    if(modifyphone==true){
+      QueryModifyPhone= "UPDATE users SET phoneNumber = "+newPhone+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyPhone, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //send user data to client
+            res.json({ "data_user": [results] });
+          }
+        }
+      });
+    }
+    if(modifypayment==true){
+      QueryModifyPayment = "UPDATE users SET paymentMethod = "+newPayment+"WHERE username = "+username+"";
+      //execute query
+      utils.pool.query(QueryModifyPayment, function (error, results, fields) {
+        if (error){
+          res.json({ "data_user": ["user not found"] });
+          throw error;
+        }
+        else{
+          if(results.length!=0){
+            //debugging
+            console.log(results)
+            //send user data to client
+            res.json({ "data_user": [results] });
+          }
+        }
+      });
+    }
+
+  }
+
+});
+
 
 //----------------- myoffers page -------------------------
 app.get("/myoffers", (req, res) => {
