bf41940002d34abbf454bfdafdac8e72a72448aa
luigibucc <102322012+luigibucc@users.noreply.github.com>
2024-01-26 16:08:45 +0100

    Add files via upload

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -14,7 +14,91 @@ const Settings = () => {
   const [pay, setPay] = useState('');
   const [username, setUsername] = useState('');
   const [mode,setMode] = useState('');
+  const [parameter, setParameter] = useState('');
   const [counter, setCounter] = useState(0);
+  let newParameter ='1';
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const submitChange = async (event) => {
+
+    const newMode = 'modify';
+
+    if(event.target.id=='usernameSubmit'){
+      newParameter = '0';
+      console.log('Username cambiato');
+    }
+    if(event.target.id=='emailSubmit'){
+      newParameter = '1';
+      console.log('email cambiata');
+    }
+    if(event.target.id=='phoneSubmit'){
+      newParameter = '2';
+      console.log('telefono cambiato');
+    }
+    if(event.target.id=='addressSubmit'){
+      newParameter = '4';
+      console.log('Indirizzo cambiato');
+    }
+    if(event.target.id=='paymentSubmit'){
+      newParameter = '5';
+      console.log('IBAN cambiato');
+    }
+
+
+
+    try {
+      const response = await fetch('/settings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'https://localhost:8000',
+          // Add other headers if necessary
+        },
+        body: JSON.stringify({
+          mode : newMode,
+          username: username,
+          modifyParameter : newParameter,
+          newUsername : username,
+          newEmail : email,
+          newAddress : address,
+          newPhone :phone,
+          newPayment : pay,
+
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
+      const result = await response.json();
+      console.log(parameter,email,result);
+
+    } 
+    catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    
+  };
+ 
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleIbanChange = (event) => {
+    setPay(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
 
   function Titolo(){
     return (
@@ -31,7 +115,6 @@ const Settings = () => {
   <Link to="/settings">Settings</Link>
   <a href="#MyOffers">My Offers</a>
   <Link to="/myauctions">My Auctions</Link>
-  <a href="/">Home</a>
   
       <form action="/search" method="get" style={{padding:'13px'}}>
         <input type="text" id="search" name="search" placeholder="Search..." style={{borderRadius:'10px'}}/>
@@ -44,7 +127,6 @@ const Settings = () => {
 
   const Display = async () => {
 
-    setMode('enter');
     console.log(mode,username);
     setUsername(sessionStorage.getItem('userData'));
     
@@ -58,7 +140,7 @@ const Settings = () => {
           // Add other headers if necessary
         },
         body: JSON.stringify({
-          mode : mode,
+          mode : "enter",
           username: username,
         }),
       });
@@ -101,32 +183,32 @@ const Settings = () => {
 
         <div class="form-group">
           <label class="label" for="username">Username:</label>
-          <input type="text" id="username" value={username} style={{width:'300px'}} name="username" required />
-          <button type="submit" class="button" id="usernameSubmit">Change</button>
+          <input type="text" id="username" value={username} style={{width:'300px'}} onChange={handleUsernameChange} name="username" required />
+          <button type="submit" class="button" id="usernameSubmit"onClick={submitChange}>Change</button>
         </div>
 
         <div class="form-group">
           <label class="label" for="email">Email:</label>
-          <input type="email" id="email" value={email} style={{width:'300px'}} name="email" required />
-          <button type="submit" class="button" id="emailSubmit">Change</button>
+          <input type="email" id="email" value={email} style={{width:'300px'}} onChange={handleEmailChange} name="email" required />
+          <button type="submit" class="button" id="emailSubmit"onClick={submitChange}>Change</button>
         </div>
 
         <div class="form-group">
           <label class="label" for="phone">Phone Number:</label>
-          <input type="tel" id="phone"value={phone} style={{width:'300px'}} name="phone" required />
-          <button type="submit" class="button" id="phoneSubmit">Change</button>
+          <input type="tel" id="phone"value={phone} style={{width:'300px'}} onChange={handlePhoneChange} name="phone" required />
+          <button type="submit" class="button" id="phoneSubmit" onClick={submitChange}>Change</button>
         </div>
 
         <div class="form-group">
           <label class="label" for="password">Password:</label>
           <input type="password" value={password} style={{width:'300px'}} id="password" name="password" required />
-          <button type="submit" class="button" id="passwordSubmit">Change</button>
+          <button type="submit" class="button" id="passwordSubmit"onClick={submitChange}>Change</button>
         </div>
 
         <div class="form-group">
           <label class="label" for="address">Address:</label>
-          <input type="text" value={address} style={{width:'300px'}} id="address" name="address" required />
-          <button type="submit" class="button" id="addressSubmit">Change</button>
+          <input type="text" value={address} style={{width:'300px'}}  onChange={handleAddressChange}id="address" name="address" required />
+          <button type="submit" class="button" id="addressSubmit"onClick={submitChange}>Change</button>
         </div>
       </div>
 
@@ -138,8 +220,8 @@ const Settings = () => {
 
         <div class="form-group" style={{marginTop:'35px'}}>
           <label class="label" for="iban">IBAN:</label>
-          <input type="text" id="payment" value={pay} style={{width:'300px'}} name="payment" required />
-          <button type="submit" class="button" id="paymentSubmit">Change</button>
+          <input type="text" id="payment" value={pay} style={{width:'300px'}} onChange={handleIbanChange} name="payment" required />
+          <button type="submit" class="button" id="paymentSubmit"onClick={submitChange}>Change</button>
         </div>
       </div>
 
