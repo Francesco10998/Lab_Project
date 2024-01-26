ee64a550dedd7236838ea6655fd0289b6e3ea44a
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-01-26 16:52:22 +0100

    Add files via upload

---
+++ client/src/MyOffers.js
@@ -0,0 +1,108 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import './css/MyOffers.css';
+import trending from "./images/trending.png"
+import iphone from "./images/iphone.jpg"
+
+const MyOffers = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+  const [counter, setCounter] = useState(0);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('userData');
+  };
+
+  const Begin = async () => {
+    setUsername(sessionStorage.getItem('userData'));
+    try {
+      const response = await fetch('/myoffers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'https://localhost:8000',
+          // Add other headers if necessary
+        },
+        body: JSON.stringify({
+          username: username
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
+      const result = await response.json();
+      setData(result);
+
+      console.log(result.length);
+
+    } 
+    catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() =>{
+    if (counter < 2) {
+      Begin();
+      setCounter(counter + 1);
+    }
+  }, [counter]);
+
+
+  function Titolo(){
+    return (
+      <div style={{ textAlign: 'center',backgroundColor:'#333333',height:'30px'}}>
+       <img src={trending} style={{height:'100px', width:'430px', margin:'auto', marginTop:'-15px'}}></img>
+      </div>
+    
+    )
+  }
+
+  function NavbarLogged(){
+    return (<div className="topnav">
+      <a href="/" onClick={handleLogout} class="logout">Logout</a>
+      <a class="username">{username}</a>
+      <Link to="/settings">Settings</Link>
+      <a href="MyOffers">My Offers</a>
+      <a href="MyAuctions">My Auctions</a>
+      <a href="/">Home</a>
+
+      <form action="/search" method="get" style={{padding:'13px'}}>
+        <input type="text" id="search" name="search" placeholder="Search..." style={{borderRadius:'10px'}}/>
+        <input type="submit" value="Search"style={{borderRadius:'10px'}} />
+      </form>
+
+    </div>
+    )
+  }
+
+  function showAuctions(){
+    return(
+      <div id="auction-container">
+          <div class="auction-item">
+            <img src={iphone} class="image"></img>
+            <div class="item-details">
+              <h3 class="objectOffers">Iphone</h3>
+              <p class="betOffers">Leading Offer: $250</p>
+              <p class="winnerOffers">Current Winner: Fede11</p>
+              <p class="deadlineOffers">Ends in 7 hrs</p>
+            </div>
+          </div>
+      </div>   
+    )
+  }
+
+
+  return (<div>
+      <Titolo/>
+      <NavbarLogged/>
+      <h1 class="title">My Offers</h1>
+      {showAuctions()}
+  </div>
+  )
+}
+export default MyOffers;
